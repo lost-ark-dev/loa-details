2dc1ad6c0036b14d6f5ca30102773f04f8384769
Eren Kara <erenkyi0@gmail.com>
2022-05-11 21:57:52 +0300

    added changelog

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,11 @@
 <template>
-  <q-page class="flex flex-center column">
-    <div class="flex column">
-      <q-card class="my-card bg-secondary text-white">
-        <q-card-section>
-          <div class="text-h6">Damage Meter</div>
-        </q-card-section>
-
-        <q-card-section>
-          See the damage of your and your party members characters!
-        </q-card-section>
-
-        <q-separator dark />
-      </q-card>
-    </div>
+  <q-page class="flex justify-start column">
+    <q-scroll-area style="height: calc(100vh - 80px); padding: 8px 16px">
+      <ChangelogList />
+    </q-scroll-area>
   </q-page>
 </template>
+
+<script setup>
+import ChangelogList from "../components/ChangelogList.vue";
+</script>
