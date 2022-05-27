ea9f031ca00abf7d7fbc20c76d54e5d424fd333e
Eren Kara <erenkyi0@gmail.com>
2022-05-27 19:28:10 +0300

    remove unused receive channel

--- src-electron/electron-preload.js
+++ src-electron/electron-preload.js
@@ -16,7 +16,6 @@ contextBridge.exposeInMainWorld("messageApi", {
       "pcap-on-state-change",
       "pcap-on-reset-state",
       "on-settings-change",
-      "save-settings",
       "parsed-logs-list",
       "parsed-log",
     ];
