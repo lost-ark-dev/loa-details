7aaee2b085363fd73e143f40adc88db5acd4beaf
Eren Kara <erenkyi0@gmail.com>
2022-06-20 15:49:11 +0300

    update screenshot filenames date format

--- src-electron/util/screenshot.js
+++ src-electron/util/screenshot.js
@@ -8,7 +8,7 @@ const path = require("path");
 const dataRegex = /^data:.+\/(.+);base64,(.*)$/;
 
 export async function saveScreenshot(dataURL) {
-  const curDate = dayjs().format("DD-MM-YYYY_HH-mm-ss"); // '25-01-2022_12-00-00'
+  const curDate = dayjs().format("YYYY-MM-DD_HH-mm-ss"); // '2022-01-25_12-00-00'
   const screenshotPath = path.join(screenshotsFolder, `${curDate}.png`);
 
   var matches = dataURL.match(dataRegex);
