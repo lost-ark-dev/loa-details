7e637e38549ef721353b3c9e3d499af442ed8022
Eren Kara <erenkyi0@gmail.com>
2022-06-20 15:34:55 +0300

    screenshot file names are based on the screenshot time

--- src-electron/util/screenshot.js
+++ src-electron/util/screenshot.js
@@ -1,5 +1,5 @@
 import { screenshotsFolder } from "./directories";
-import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
 
 const fs = require("fs");
 const fsPromises = fs.promises;
@@ -8,7 +8,8 @@ const path = require("path");
 const dataRegex = /^data:.+\/(.+);base64,(.*)$/;
 
 export async function saveScreenshot(dataURL) {
-  const screenshotPath = path.join(screenshotsFolder, `${uuidv4()}.png`);
+  const curDate = dayjs().format("DD-MM-YYYY_HH-mm-ss"); // '25-01-2022_12-00-00'
+  const screenshotPath = path.join(screenshotsFolder, `${curDate}.png`);
 
   var matches = dataURL.match(dataRegex);
   var data = matches[2];
