ae99aca95be59773ba5b72cf78bb3f86405bfd45
Herysia <totogirerd@gmail.com>
2023-09-20 20:37:39 +0200

    Game update - 20/09/2023 - Weekly maintenance

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -60,7 +60,11 @@
       </p>
 
       <h5>Changelog</h5>
-      <h6 id="1-4-11">1.4.11</h6>
+      <h6 id="1-4-13">1.4.13</h6>
+      <ul>
+        <li>Support for game update - 20/09/2023 (Weekly maintenance)</li>
+      </ul>
+      <h6 id="1-4-12">1.4.12</h6>
       <ul>
         <li>Fix invalid projectileId</li>
       </ul>
