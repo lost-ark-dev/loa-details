79c71b8a675d70146ee3efd466ea917065ebef92
Herysia <totogirerd@gmail.com>
2023-12-13 21:47:13 +0100

    Update 13/12/2023 - Voldis

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -60,6 +60,10 @@
       </p>
 
       <h5>Changelog</h5>
+      <h6 id="1-4-19">1.4.19</h6>
+      <ul>
+        <li>Support for game update - 13/12/2023 (Voldis)</li>
+      </ul>
       <h6 id="1-4-18">1.4.18</h6>
       <ul>
         <li>Fix slayer icon</li>
